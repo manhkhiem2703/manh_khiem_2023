05e54719042ca758b0fda3c7323ee30c9a1f52da
Manh Khiem <manhkhiem2703@gmail.com>
2023-11-10 01:03:35 +0700

    last-commit

--- client/src/components/home/post/Post.jsx
+++ client/src/components/home/post/Post.jsx
@@ -39,8 +39,8 @@ const Details = styled(Typography)`
 const Post = ({ post }) => {
     const url = post.picture ? post.picture : 'https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=752&q=80';
     
-    const addEllipsis = (str, limit) => {
-        return str.length > limit ? str.substring(0, limit) + '...' : str;
+    const addEllipsis = (str) => {
+        return str.length > 100 ? str.substring(0, 100) + '...' : str;
     } 
 
     return (
